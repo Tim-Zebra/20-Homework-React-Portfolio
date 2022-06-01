28542cbef016424bdfaaf93e2fd01348a34412a2
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-01 08:48:24 -0500

    able to add link for reveal, aligned text as desired

--- my-app/src/components/pages/portfolio/Project.js
+++ my-app/src/components/pages/portfolio/Project.js
@@ -4,14 +4,23 @@
 // When mouse hovers over:
 // Fade image, show Name (link to deployed page), Github Icon (link to git hub), name of programs used (eg. MERN stack )
 // Need 6 apps
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Project({currentProject}) {
-  console.log('This happened', currentProject.image);
+// Mouse enter effects
+  const [isShown, setIsShown] = useState(false);
   return (
-    <div className='col-6 my-3 projectDiv'>
-      <img src={currentProject.image} alt="project screen shot" className="projectBackground"></img>
-      <a href={currentProject.link} className="linkProjectProgramsUsed hide"><div className="projectProgramsUsed">{currentProject.prgms}</div></a>
+    <div className='col-6 my-3 projectDiv'
+    onMouseEnter={() => setIsShown(true)}
+    onMouseLeave={() => setIsShown(false)}>
+      <img src={currentProject.image} alt="project screen shot" className="projectBackground">
+      </img>
+      {isShown &&
+        <div className="projectProgramsUsed text-center">{currentProject.prgms}</div>
+      }
+      {isShown && currentProject.deployed !== false &&
+        <div className="text-center">{currentProject.prgms}</div>
+      }
     </div>
   );
 }
