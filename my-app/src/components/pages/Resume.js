4d9d873725748cf27bf8953741c6655276ec6bf0
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-02 15:44:40 -0500

    able to affect Download Resume Here

--- my-app/src/components/pages/Resume.js
+++ my-app/src/components/pages/Resume.js
@@ -3,18 +3,41 @@
 // List of proficiences
 // Front End
 // Back End
-import React from 'react';
+import React, {useState} from 'react';
 import pdf from '../../assets/resume.pdf';
 
 export default function Resume() {
+// Styles for resume
+const noStyle = {
+  transitionDuration: '0.5s',
+};
+
+const linkStyle = {
+  color: 'red',
+  fontSize: '40px',
+  transitionDuration: '2s',
+}
+
+const [ resumeStyle, setResumeStyle] = useState(noStyle);
+
+const resumeStyleChangeActive = () => {
+  setResumeStyle(linkStyle);
+}
+
+const resumeStyleChangeInactive = () => {
+  setResumeStyle(noStyle);
+}
+
   return (
     <section className="portfolioSection">
       <div className="d-block mb-5 text-center">
         <h1>Resume</h1>
-        <div>Download my Resume <a href={pdf} target="_blank" rel="noreferrer">Here</a></div>
+        <div>Download my Resume <a href={pdf} target="_blank" rel="noreferrer" id="resumeLink" style={resumeStyle}>Here</a></div>
       </div>
       <div id="resumeBlock" className="d-flex row col-12 ml-4 justify-content-around proficienciesBlock">
-        <div id="frontendProficiences" className="col-lg-4 col-md-6 align-center resumeProficiences">
+        <div id="frontendProficiences" className="col-lg-4 col-md-6 align-center resumeProficiences"
+        onMouseEnter={() => resumeStyleChangeActive()}
+        onMouseLeave={() => resumeStyleChangeInactive()}>
           <h4>
             Front-End Proficiences
             <ul>
@@ -28,7 +51,9 @@ export default function Resume() {
             </ul>
           </h4>
         </div>
-        <div id="backendProficiences" className="col-lg-4 col-md-6 align-center resumeProficiences">
+        <div id="backendProficiences" className="col-lg-4 col-md-6 align-center resumeProficiences"
+        onMouseEnter={() => resumeStyleChangeActive()}
+        onMouseLeave={() => resumeStyleChangeInactive()}>
           <h4>
             Back-End Proficiences
             <ul>
