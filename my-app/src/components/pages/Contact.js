27f9c408b5c156dcd03993c05f534cc9b636d36e
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-02 09:53:55 -0500

    added pseudocode for mousing events

--- my-app/src/components/pages/Contact.js
+++ my-app/src/components/pages/Contact.js
@@ -14,10 +14,17 @@ export default function Contact() {
 
   }
 
-  // Allows for active changing of variables
+  // Variables for validating content
   const [ isEmail, setIsEmail ] = useState(false);
   const [ isUserName, setIsUsername ] = useState(false);
   const [ isMessage, setIsMessage ] = useState(false);
+
+  // Variables for input content present
+  const [ emailInput, setemailInput ] = useState('');
+  const [ usernameInput, setusernameInput ] = useState('');
+  const [ messageInput, setmessageInput ] = useState('');
+
+  const 
 
   // Handle Mouse Over
   const handleMouseOver = () => {
@@ -26,10 +33,6 @@ export default function Contact() {
   }
 
   // Handle form submit
-  // Makes sure username is entered
-  const validateUsername = (user) => {
-
-  }
   // Makes sure email is in the proper format
   const validateEmail = (email) => {
     // Regex to confirm email format
@@ -41,13 +44,12 @@ export default function Contact() {
 
   }
 
-  // Makes sure message is entered
-  const validateUsername = (name) => {
-
+  // Makes sure content is entered
+  const validateContent = (content) => {
+    return content !== null || undefined ? true:false;
   }
 
   const validate = () => {
-
 
     // Resets fields if valid info
     if(isUsername && isEmail && isMessage) {
@@ -67,23 +69,23 @@ export default function Contact() {
             onMouseEnter={() => setIsShown(true)}
             onMouseLeave={() => setIsShown(false)}>
             <label htmlFor="name">Name
-            {if <span></span>
+            <span></span>
             </label>
             <input type="text" className="form-control">
 
             </input>
           </div>
           <div className="form-group"
-          onMouseEnter={() => setIsShown(true)}
-          onMouseLeave={() => setIsShown(false)}>
+            onMouseEnter={() => setIsShown(true)}
+            onMouseLeave={() => setIsShown(false)}>
             <label htmlFor="exampleInputEmail1">Email address</label>
             <input type="text" className="form-control">
 
             </input>
           </div>
           <div className="form-group"
-          onMouseEnter={() => setIsShown(true)}
-          onMouseLeave={() => setIsShown(false)}>
+            onMouseEnter={() => setIsShown(true)}
+            onMouseLeave={() => setIsShown(false)}>
             <label htmlFor="message">Message</label>
             <textarea className="form-control" rows="5">
             
